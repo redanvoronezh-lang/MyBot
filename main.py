a6347e2c790de2a47196f1c1f9492d552075771d
redanvoronezh-lang <redanvoronezh@gmail.com>
2026-04-20 18:33:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
-from aiogram.types import WebAppInfo
+from aiogram.types import WebAppInfo, PreCheckoutQuery, LabeledPrice
 
-# Твои данные
 TOKEN = '8671015752:AAEsttbvnv698gP3D3vO0TtkkrZCovSU2uY'
 APP_URL = 'https://redanvoronezh-lang.github.io/MyBot/'
 
@@ -12,20 +11,33 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    # Создаем кнопку под полем ввода
     markup = types.ReplyKeyboardMarkup(
-        keyboard=[
-            [types.KeyboardButton(text="🎁 Открыть LapiGift", web_app=WebAppInfo(url=APP_URL))]
-        ],
+        keyboard=[[types.KeyboardButton(text="🎁 Открыть LapiGift", web_app=WebAppInfo(url=APP_URL))]],
         resize_keyboard=True
     )
-    await message.answer(
-        f"Привет, {message.from_user.first_name}!\n\nДобро пожаловать. Нажми на кнопку ниже, чтобы запустить приложение:",
-        reply_markup=markup
+    await message.answer(f"Привет, {message.from_user.first_name}!", reply_markup=markup)
+
+@dp.message(F.web_app_data.data == "pay_stars")
+async def create_invoice(message: types.Message):
+    await bot.send_invoice(
+        chat_id=message.chat.id,
+        title="Пополнение",
+        description="Зачисление 10 💎",
+        provider_token="",
+        currency="XTR",
+        prices=[LabeledPrice(label="XTR", amount=10)],
+        payload=f"refill_{message.from_user.id}"
     )
 
+@dp.pre_checkout_query()
+async def process_pre_checkout(query: PreCheckoutQuery):
+    await bot.answer_pre_checkout_query(query.id, ok=True)
+
+@dp.message(F.successful_payment)
+async def success_pay(message: types.Message):
+    await message.answer(f"✅ Зачислено: {message.successful_payment.total_amount} 💎")
+
 async def main():
-    # Запуск бота
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
