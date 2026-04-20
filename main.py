6b5bb91c08c5e46a97d3bf58001aa8900bc64d49
redanvoronezh-lang <redanvoronezh@gmail.com>
2026-04-20 19:39:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-from aiogram.types import WebAppInfo
+from aiogram.types import WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
 
-# Твой токен и инициализация
-TOKEN = "8671015752:AAEsttbvnv698gP3D3vO0TtkkrZCovSU2uY"
-bot = Bot(token=TOKEN)
+bot = Bot(token="8671015752:AAEsttbvnv698gP3D3vO0TtkkrZCovSU2uY")
 dp = Dispatcher()
 
 @dp.message(CommandStart())
-async def start(message: types.Message):
-    # Кнопки
-    kb = [
-        [types.InlineKeyboardButton(text="🚀 Играть", web_app=WebAppInfo(url="https://redanvoronezh-lang.github.io/MyBot/"))],
-        [types.InlineKeyboardButton(text="📢 Канал", url="https://t.me/LapiGift")],
-        [types.InlineKeyboardButton(text="🎧 Поддержка", url="https://t.me/LapiGift_sup_bot")]
-    ]
-    markup = types.InlineKeyboardMarkup(inline_keyboard=kb)
-
-    # Текст сообщения
+async def start(m: types.Message):
+    kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="🚀 Играть", web_app=WebAppInfo(url="https://redanvoronezh-lang.github.io/MyBot/"))],
+        [InlineKeyboardButton(text="📢 Канал", url="https://t.me/LapiGift")],
+        [InlineKeyboardButton(text="🎧 Поддержка", url="https://t.me/LapiGift_sup_bot")]
+    ])
+    
     text = (
-        "🎉 Привет, на связи команда **LapiGift** и теперь ты в нашей большой семье! 🎁\n\n"
+        "🎉 Привет, на связи команда LapiGift и теперь ты в нашей большой семье! 🎁\n\n"
         "Открывай кейсы и выигрывай лучшие NFT гифты!\n\n"
         "💰 Делись своей реферальной ссылкой с друзьями – и за каждого приведённого друга "
         "который сделает депозит ты получишь 5% от суммы их пополнений!\n"
@@ -28,8 +23,8 @@ async def start(message: types.Message):
         "🎁 Хочешь попробовать?\n"
         "Жми «Играть» и забирай свой приз!"
     )
-
-    await message.answer(text, reply_markup=markup, parse_mode="Markdown")
+    
+    await m.answer(text, reply_markup=kb)
 
 async def main():
     await dp.start_polling(bot)
